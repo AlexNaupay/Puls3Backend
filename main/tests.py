3d209564c0e25c489c7bd1cffc22e96cde8c994f
Alex Naupay <alexnaupay@gmail.com>
2014-02-14 00:24:04 -0500

    Pruebas

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 # Create your tests here.
+class SimpleTest(TestCase):
+	def test_home(self):
+		res = self.client.get('/')
+		self.assertEqual(res.status_code,200)
+		res = self.client.get(reverse('home'))
+		self.assertEqual(res.status_code,200)
+		res = self.client.get(reverse('home2'))
+		self.assertEqual(res.status_code,200)
